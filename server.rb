d96797e7d40cc501c418c9efd86d0d12a2b9eaf5
Daniel <danielhebrink@britecore.com>
2019-06-25 14:06:13 -0500

    Add open-reopened PR handlers

--- server.rb
+++ server.rb
@@ -6,9 +6,23 @@ require 'openssl'     # Verifies the webhook signature
 require 'jwt'         # Authenticates a GitHub App
 require 'time'        # Gets ISO 8601 representation of a Time object
 require 'logger'      # Logs debug statements
+require 'httparty'    # Sending additional API requests
+require 'base64'
 
 set :port, 3000
 set :bind, '0.0.0.0'
+
+CONFIG_FILE = '.file-checker.json'
+CONFIG_KEY = 'filename'
+GITHUB_API = 'https://api.github.com/'
+
+
+class MyRequest
+  include HTTParty
+  # TODO: This isn't authenticating correctly...
+  headers('Authorization' => '60fd0f91763f2f240be028bb2cc4058a6b379d44',
+          'User-Agent' => 'Hebrink First Test App')
+end
 
 
 # This is template code to create a GitHub App server.
@@ -62,7 +76,19 @@ class GHAapp < Sinatra::Application
     # ADD YOUR CODE HERE  #
     # # # # # # # # # # # #
 
-    200 # success status
+    the_event = request.env['HTTP_X_GITHUB_EVENT']
+    the_action = @payload['action']
+    unknown_message = 'Unknown %s action: %s' % [the_event, the_action]
+    case the_event
+    when 'pull_request'
+      if the_action === 'opened' || the_action === 'reopened'
+        handle_opened_pr(@payload)
+      else
+        logger.debug(unknown_message)
+      end
+    else
+        logger.debug(unknown_message)
+    end
   end
 
 
@@ -71,6 +97,54 @@ class GHAapp < Sinatra::Application
     # # # # # # # # # # # # # # # # #
     # ADD YOUR HELPER METHODS HERE  #
     # # # # # # # # # # # # # # # # #
+  
+    def handle_opened_pr(the_payload)
+      pr_number = the_payload['pull_request']['number']
+      repo_name = the_payload['repository']['full_name']
+
+      # TODO: Get the config file from the repo's MASTER branch that tells us which file to check for.
+      file_of_interest = 'required_file_to_change.py'
+      #file_of_interest = read_from_repo_config(repo_name)
+
+      # Grab the files changed in the PR and see if one of them is our target file.
+      found = false
+      diff_files = @installation_client.pull_request_files(repo_name, pr_number)
+      for f in diff_files do
+        logger.debug('%s found as changed' % [f['filename']])
+        if f['filename'] == file_of_interest
+            found = true
+            break
+        end
+      end
+
+      if !found
+        comment = 'Review your changes that have been submitted. The following file is required to be updated: `%s`' % [file_of_interest]
+        api_options = {'body' => comment, 'event' => 'COMMENT'}
+        @installation_client.create_pull_request_review(repo_name, pr_number, api_options)
+      end
+    end
+
+    def read_from_repo_config(repo_name)
+      target_file = nil
+      repo_details = @installation_client.repository(repo_name)
+      short_name = repo_details['name']
+      owner_user = repo_details['owner']['login']
+      content_path = 'repos/%{owner}/%{repo}/%{config_file}' % { :owner => owner_user, :repo => short_name, :config_file => CONFIG_FILE }
+      # TODO: This is failing with 404 due to lack of authorization... Not sure what to do here at this time.
+      response = MyRequest.get(GITHUB_API + content_path)
+      if response.code == 200
+        file_content = JSON.parse(response.body)
+        file_json = Base64.decode64(file_content['content'])
+        file_json = JSON.parse(file_json)
+        target_file = file_json[CONFIG_KEY]
+      else
+        logger.debug('Content Request Response: ' + response.code)
+        logger.debug(response.body)
+      end
+      return target_file
+    end
+
+    ############## BEGIN - template helper functions
 
     # Saves the raw payload and converts the payload to JSON format
     def get_payload_request(request)
